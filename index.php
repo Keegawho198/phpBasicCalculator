be2084d897c65f802d99396e7d8e9083cb9310bc
Keegan Nair <Keegan.nair@hotmail.com>
2020-11-23 23:30:16 +1100

    Adding code files

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  <body>
+
+    <form action="functions.php" method="get">
+      <input type="text" name="num01" placeholder="Number 1">
+      <select name="oper">
+        <label>Choose operation!</lable>
+          <option value="add">Add </option>
+          <option value="sub">Subtract </option>
+      </select>
+      <input type="text" name="num02" placeholder="Number 2">
+      <button type="submit">calculate!</button>
+    </form>
+
+
+  </body>
+</html>
+
+
+
+<!-- basic calculator using php -->
